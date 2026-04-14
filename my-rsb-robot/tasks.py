8595c393db3ce5095ba1a855381e48d9e403a6aa
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-14 17:01:13 +0200

    Step 2 - Logging into the intranet

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -5,10 +5,18 @@ from robocorp import browser
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
-        slowmo=100,
+        slowmo=1000,
     )
     open_the_intranet_website()
+    log_in()
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
     browser.goto("https://robotsparebinindustries.com/")
+
+def log_in():
+    """Fills in the login form and clicks the 'Log in' button"""
+    page = browser.page()
+    page.fill("#username", "maria")
+    page.fill("#password", "thoushallnotpass")
+    page.click("button:text('Log in')")
