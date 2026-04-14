c8f70ef00f3573f18f54fd3c5722aced039a4805
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-14 16:48:40 +0200

    Step 1 - Geting to the intranet

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -1,6 +1,14 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def robot_spare_bin_python():
+    """Insert the sales data for the week and export it as a PDF"""
+    browser.configure(
+        slowmo=100,
+    )
+    open_the_intranet_website()
+
+def open_the_intranet_website():
+    """Navigates to the given URL"""
+    browser.goto("https://robotsparebinindustries.com/")
