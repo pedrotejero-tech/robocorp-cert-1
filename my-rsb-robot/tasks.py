fb214e574c54efc8c8f8ea142cc814a0fe35db52
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-15 12:07:33 +0200

    Step 5 - Using Excel data to fill in the form

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -2,17 +2,18 @@ from robocorp.tasks import task
 from robocorp import browser
 
 from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
 
 @task
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
-        slowmo=500,
+        slowmo=100,
     )
     open_the_intranet_website()
     log_in()
     download_excel_file()
-    fill_and_submit_sales_form()
+    fill_form_with_excel_data()
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
@@ -25,17 +26,27 @@ def log_in():
     page.fill("#password", "thoushallnotpass")
     page.click("button:text('Log in')")
 
-def fill_and_submit_sales_form():
+def fill_and_submit_sales_form(sales_rep):
     """Fills in the sales data and click the 'Submit' button"""
     page = browser.page()
 
-    page.fill("#firstname", "John")
-    page.fill("#lastname", "Smith")
-    page.fill("#salesresult", "123")
-    page.select_option("#salestarget", "10000")
+    page.fill("#firstname", sales_rep["First Name"])
+    page.fill("#lastname", sales_rep["Last Name"])
+    page.select_option("#salestarget", str(sales_rep["Sales Target"]))
+    page.fill("#salesresult", str(sales_rep["Sales"]))
     page.click("text=Submit")
 
 def download_excel_file():
     """Downloads excel file from the given URL"""
     http = HTTP()
     http.download(url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
+
+def fill_form_with_excel_data():
+    """Read data from excel and fill in the sales form"""
+    excel = Files()
+    excel.open_workbook("SalesData.xlsx")
+    worksheet = excel.read_worksheet_as_table("data", header=True)
+    excel.close_workbook()
+
+    for row in worksheet:
+        fill_and_submit_sales_form(row)
