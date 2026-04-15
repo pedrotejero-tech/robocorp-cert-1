9157eb190eadba1a9eefd3e55e483adf26c946e1
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-15 12:53:11 +0200

    Step 8 - Creating a PDF with the results table

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -3,18 +3,20 @@ from robocorp import browser
 
 from RPA.HTTP import HTTP
 from RPA.Excel.Files import Files
+from RPA.PDF import PDF
 
 @task
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
-        slowmo=100,
+        slowmo=10,
     )
     open_the_intranet_website()
     log_in()
     download_excel_file()
     fill_form_with_excel_data()
     collect_results()
+    export_as_pdf()
     log_out()
 
 def open_the_intranet_website():
@@ -57,6 +59,14 @@ def collect_results():
     """Take a screenshot of the page"""
     page = browser.page()
     page.screenshot(path="output/sales_summary.png")
+
+def export_as_pdf():
+    """Export the data to a pdf file"""
+    page = browser.page()
+    sales_results_html = page.locator("#sales-results").inner_html()
+    
+    pdf = PDF()
+    pdf.html_to_pdf(sales_results_html, "output/sales_results.pdf")
 
 def log_out():
     """Presses the 'Log out' button"""
